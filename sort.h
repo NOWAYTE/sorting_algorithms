5f82587e5528a68a993ca40ff4702a3289aa9821
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-09-27 02:23:20 +0300

    A function that sorts a doubly linked list

--- sort.h
+++ sort.h
@@ -31,6 +31,9 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void print_list(const listint_t *list);
+void s_node(listint_t **n, listint_t **n1, listint_t *n2);
 
 
 #endif
